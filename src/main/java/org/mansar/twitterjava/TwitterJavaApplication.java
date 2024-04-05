21515a98d9a6cc71547c30736b6ab9ab7e76309b
Abdeddaim Mansar <mansarabdeddaim12@gmaiL.com>
2024-04-05 11:02:15 +0000

    feat: initial commit

---
+++ src/main/java/org/mansar/twitterjava/TwitterJavaApplication.java
@@ -0,0 +1,13 @@
+package org.mansar.twitterjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mansar/twitterjava/TwitterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mansar.twitterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
